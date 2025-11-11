c03dd267a7aa365addaa383d047dcff4987d990e
Anthony Ismael Cabrera González <144255723+Anthonycabr@users.noreply.github.com>
2025-11-11 00:03:45 -0400

    vamo a ver

--- RestaurantPMS/Models/CocinaViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantPMS.Models
-{
-    public class CocinaViewModel
-    {
-        public List<Order> Pendientes { get; set; } = new();
-        public List<Order> Preparando { get; set; } = new();
-        public List<Order> Listos { get; set; } = new();
-    }
-}
